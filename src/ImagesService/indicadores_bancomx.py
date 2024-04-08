2b1d41cbf264491c5efb86476b903453b79a1768
Octavio Zenil <octaviozenil@gmail.com>
2024-04-08 15:39:48 -0700

    First stable version

--- src/ImagesService/indicadores_bancomx.py
+++ src/ImagesService/indicadores_bancomx.py
@@ -1,7 +1,12 @@
 import requests
 
+def get_services():
+    url = 'http://web_image:8000/services/'
+    response = requests.get(url)
+    return response.json()
+
 def get_indicadores_bamx() -> bytes:
-    url = 'http://172.17.0.4:8000/services/get_image/indicadores_bancomx'
+    url = 'http://web_image:8000/services/get_image/indicadores_bancomx'
     response = requests.put(url, json={
         "context": {}
     })
--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 import schedule
+import time
 from tasks import get_indicadores_bamx
+from ImagesService.indicadores_bancomx import get_services
+
+print('Response from the server: ', get_services())
 
 schedule.every().friday.at("15:00").do(get_indicadores_bamx)
 
-schedule.run_pending()
+while True:
+    schedule.run_pending()
+    time.sleep(1)
